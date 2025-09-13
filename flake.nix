042d590f9ee3b7e0dda89e97d69a20496e7ecef5
AbrasiveAlmond <qpearson.nz@gmail.com>
2025-09-13 12:29:51 +1200

    add ty

--- flake.nix
+++ flake.nix
@@ -4,28 +4,34 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-  flake-utils.lib.eachDefaultSystem
-  (system:
-    let
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-
-    in
-    with pkgs;
+  outputs =
     {
-      devShells.default = mkShell {
-        packages = [
-          (pkgs.python3.withPackages (python-pkgs: [
-            python-pkgs.numpy
-            python-pkgs.matplotlib
-            python-pkgs.uv
-            python-pkgs.uv-build
-            python-pkgs.ruff
-          ]))
-        ];
-      };
-    }
-  );
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          packages = [
+            (pkgs.python3.withPackages (python-pkgs: [
+              python-pkgs.numpy
+              python-pkgs.matplotlib
+              python-pkgs.uv
+              python-pkgs.uv-build
+              python-pkgs.ruff
+            ]))
+            pkgs.ty
+          ];
+        };
+      }
+    );
 }
